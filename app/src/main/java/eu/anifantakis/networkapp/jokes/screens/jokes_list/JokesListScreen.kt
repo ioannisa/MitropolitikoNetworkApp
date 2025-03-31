2d218d861f5d49a115a0502c340791a0c0c532ac
Ioannis Anifantakis <ianyfantakis@deloitte.gr>
2025-03-31 14:43:37 +0300

    Add Refresh button

--- app/src/main/java/eu/anifantakis/networkapp/jokes/screens/jokes_list/JokesListScreen.kt
+++ app/src/main/java/eu/anifantakis/networkapp/jokes/screens/jokes_list/JokesListScreen.kt
@@ -2,7 +2,6 @@ package eu.anifantakis.networkapp.jokes.screens.jokes_list
 
 import android.widget.Toast
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,13 +9,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -73,47 +72,48 @@ private fun JokesListScreen(
     state: JokesListState,
     onIntent: (JokesListIntent) -> Unit
 ) {
-
-    Box(
+    Column(
         modifier = modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+            .padding(horizontal = 16.dp)
+            .fillMaxSize()
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
+        Button(
+            onClick = {
+                onIntent(JokesListIntent.Refresh)
+            }
         ) {
-            LazyColumn {
-                items(
-                    items = state.jokes,
-                    key = { it.id }
+            Text("Refresh")
+        }
+
+        LazyColumn {
+            items(
+                items = state.jokes,
+                key = { it.id }
+            ) {
+                Card(
+                    modifier = Modifier
+                        .padding(vertical = 4.dp)
                 ) {
-                    Card(
+                    Row(
                         modifier = Modifier
-                            .padding(horizontal = 8.dp, vertical = 4.dp)
-                    ) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable {
-                                    onIntent(JokesListIntent.ClickOnJoke(it))
-                                }
-                        ) {
-                            Column(
-                                modifier = Modifier.padding(16.dp)
-                            ) {
-                                Text(text = it.question, fontWeight = FontWeight.Bold)
-                                Text(text = it.answer)
+                            .fillMaxWidth()
+                            .clickable {
+                                onIntent(JokesListIntent.ClickOnJoke(it))
                             }
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Text(text = it.question, fontWeight = FontWeight.Bold)
+                            Text(text = it.answer)
                         }
                     }
                 }
             }
         }
-
-        if (state.loading) {
-            CircularProgressIndicator()
-        }
     }
 
+    if (state.loading) {
+        CircularProgressIndicator()
+    }
 }
