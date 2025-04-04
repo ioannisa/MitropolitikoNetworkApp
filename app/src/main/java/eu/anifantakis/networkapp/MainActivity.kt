2a2860d92656ca1b20ff447b375b409cd0960c33
Ioannis Anifantakis <ianyfantakis@deloitte.gr>
2025-04-05 00:43:14 +0300

    Transferred DI Initialization into MyApplication.kt

--- app/src/main/java/eu/anifantakis/networkapp/MainActivity.kt
+++ app/src/main/java/eu/anifantakis/networkapp/MainActivity.kt
@@ -7,20 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import eu.anifantakis.networkapp.jokes.data.di.AppModule
 import eu.anifantakis.networkapp.ui.theme.NetworkAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Initialize our dependencies
-        AppModule.initialize(applicationContext)
-
         enableEdgeToEdge()
         setContent {
             NetworkAppTheme {
