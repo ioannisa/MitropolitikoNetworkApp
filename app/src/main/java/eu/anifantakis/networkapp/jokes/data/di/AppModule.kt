570aeb36aa784cac768f4373e4fe7ef594de771b
Ioannis Anifantakis <ianyfantakis@novibet.com>
2026-03-30 18:03:06 +0300

    Refactor JokesDatabase initialization to use Room's databaseBuilder for improved instance management

--- app/src/main/java/eu/anifantakis/networkapp/jokes/data/di/AppModule.kt
+++ app/src/main/java/eu/anifantakis/networkapp/jokes/data/di/AppModule.kt
@@ -1,6 +1,7 @@
 package eu.anifantakis.networkapp.jokes.data.di
 
 import android.content.Context
+import androidx.room.Room
 import eu.anifantakis.networkapp.jokes.data.JokesRepository
 import eu.anifantakis.networkapp.jokes.data.database.JokesDatabase
 import eu.anifantakis.networkapp.jokes.data.network.KtorClient
@@ -21,7 +22,11 @@ object AppModule {
     }
 
     val jokesDatabase: JokesDatabase by lazy {
-        JokesDatabase.getDatabase(appContext)
+        Room.databaseBuilder(
+            appContext,
+            JokesDatabase::class.java,
+            "jokes_database"
+        ).build()
     }
 
     // FACTORY: Using get() means a new instance is created on every call.
